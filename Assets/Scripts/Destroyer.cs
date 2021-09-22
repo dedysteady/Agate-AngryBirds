18a71fdf7ca49a0a17e3b6f5add7a76c6a6fbba4
dedysteady <dedysetiadi1708@gmail.com>
2021-09-22 18:52:40 +0700

    First commit

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+
+    private bool _isGameEnded = false;
+
+
+    void Start()
+    {
+        for(int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    void OnMouseUp()
+    {
+        if(_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if(Birds.Count > 0)
+            SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            if(Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if(Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+}
